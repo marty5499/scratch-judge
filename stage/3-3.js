a30fcb89106fafdeb599e937d2474bcf4bd196ff
marty <marty.shu@gmail.com>
2024-06-07 13:35:09 +0800

    fix 3-3 ok

--- stage/3-3.js
+++ stage/3-3.js
@@ -7,19 +7,25 @@ window.TestCase = class TestCase {
   async start(callback) {
     this.callback = callback;
     await this.case01();
+    await this.case02();
   }
 
-  async case01() { 
+  async case01() {
     var sprite1 = this.judge.sprites[Object.keys(this.judge.sprites)[2]];
     var sprite2 = this.judge.sprites[Object.keys(this.judge.sprites)[2]];
     await this.judge.delay(150);
     this.judge.press("ArrowUp", 200);
     await this.judge.press(" ", 10);
-    await this.judge.delay(300);
-    console.log("sprite1:", this.judge.sprites);
-    console.log("sprite1:", sprite1);
-    this.callback("case01", true, "主角攻擊敵人");
-    //sprite1["records"] = [];
-    //sprite2["records"] = [];
+    await this.judge.delay(200);
+    window.ss = this.judge.sprites;
+    var condition = Object.keys(this.judge.sprites).length >= 4;
+    this.callback("case01", condition, "主角攻擊敵人");
+  }
+
+  async case02() {
+    var catCollisionTimes = this.judge.collisionCounts["Cat"];
+    var dragonCollisionTimes = this.judge.collisionCounts["Dragon"];
+    var condition = catCollisionTimes >= 2 && dragonCollisionTimes == 1;
+    this.callback("case02", condition, "主角碰撞");
   }
 };
